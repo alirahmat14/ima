8e20f33d3ef6b8e3a88b8ebbba2175ee8910a16b
alirahmat14 <4lirahmath@gmail.com>
2024-03-11 15:26:36 +0700

    update section masalah

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,22 +44,22 @@
   /**
    * Navbar links active state on scroll
    */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
+  // let navbarlinks = select('#navbar .scrollto', true)
+  // const navbarlinksActive = () => {
+  //   let position = window.scrollY + 200
+  //   navbarlinks.forEach(navbarlink => {
+  //     if (!navbarlink.hash) return
+  //     let section = select(navbarlink.hash)
+  //     if (!section) return
+  //     if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+  //       navbarlink.classList.add('active')
+  //     } else {
+  //       navbarlink.classList.remove('active')
+  //     }
+  //   })
+  // }
+  // window.addEventListener('load', navbarlinksActive)
+  // onscroll(document, navbarlinksActive)
 
   /**
    * Scrolls to an element with header offset
@@ -79,29 +79,40 @@
     })
   }
 
+  let x =window.matchMedia("(max-width: 1024px)");
+  let selectHeader = select('#header');
+  let selectOnlineNav = select('#online-trial-nav');
+  if (x.matches) {
+    selectHeader.classList.add('bg-dark');
+    selectHeader.classList.add('bg-opacity-75');
+  } else {
+    selectOnlineNav.classList.add('text-primary-emphasis')
+  }
+
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
-  let x =window.matchMedia("(max-width: 992px)");
-  let selectHeader = select('#header');
-  let selectOnlineNav = select('#online-trial-nav');
+  // let x =window.matchMedia("(max-width: 992px)");
+  
   if (selectHeader) {
     const headerScrolled = () => {
       if (x.matches) {
         selectHeader.classList.add('bg-dark');
         selectHeader.classList.add('bg-opacity-75');
       } else {
-        selectOnlineNav.classList.add('text-primary-emphasis')
+        // selectOnlineNav.classList.add('text-primary-emphasis')
         if (window.scrollY > 10) {
           selectHeader.classList.remove('bg-transparent')
-          selectHeader.classList.add('bg-opacity-25')
+          selectHeader.classList.add('bg-opacity-50')
           selectHeader.classList.add('bg-dark')
           selectHeader.classList.add('shadow')
+          selectOnlineNav.classList.remove('text-primary-emphasis')
         } else {
           selectHeader.classList.remove('bg-dark')
           selectHeader.classList.remove('shadow')
-          selectHeader.classList.remove('bg-opacity-25')
+          selectHeader.classList.remove('bg-opacity-50')
           selectHeader.classList.add('bg-transparent')
+          selectOnlineNav.classList.add('text-primary-emphasis')
         }
       }
       
@@ -129,60 +140,60 @@
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+  // on('click', '.mobile-nav-toggle', function(e) {
+  //   select('#navbar').classList.toggle('navbar-mobile')
+  //   this.classList.toggle('bi-list')
+  //   this.classList.toggle('bi-x')
+  // })
 
   /**
    * Mobile nav dropdowns activate
    */
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
+  // on('click', '.navbar .dropdown > a', function(e) {
+  //   if (select('#navbar').classList.contains('navbar-mobile')) {
+  //     e.preventDefault()
+  //     this.nextElementSibling.classList.toggle('dropdown-active')
+  //   }
+  // }, true)
 
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
+  // on('click', '.scrollto', function(e) {
+  //   if (select(this.hash)) {
+  //     e.preventDefault()
 
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
-      scrollto(this.hash)
-    }
-  }, true)
+  //     let navbar = select('#navbar')
+  //     if (navbar.classList.contains('navbar-mobile')) {
+  //       navbar.classList.remove('navbar-mobile')
+  //       let navbarToggle = select('.mobile-nav-toggle')
+  //       navbarToggle.classList.toggle('bi-list')
+  //       navbarToggle.classList.toggle('bi-x')
+  //     }
+  //     scrollto(this.hash)
+  //   }
+  // }, true)
 
   /**
    * Scroll with ofset on page load with hash links in the url
    */
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash)
-      }
-    }
-  });
+  // window.addEventListener('load', () => {
+  //   if (window.location.hash) {
+  //     if (select(window.location.hash)) {
+  //       scrollto(window.location.hash)
+  //     }
+  //   }
+  // });
 
   /**
    * Preloader
    */
-  let preloader = select('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove()
-    });
-  }
+  // let preloader = select('#preloader');
+  // if (preloader) {
+  //   window.addEventListener('load', () => {
+  //     preloader.remove()
+  //   });
+  // }
 
   /**
    * Initiate glightbox
@@ -221,6 +232,8 @@
    */
   window.addEventListener('load', () => {
     AOS.init({
+      disable: 'mobile',
+      disable: 'tablet',
       duration: 1000,
       easing: 'ease-in-out',
       once: true,
